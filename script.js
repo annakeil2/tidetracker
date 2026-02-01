0debb9ca46696eeb8cbb434b6478433e5caa7286
Anna Keil <keilanna0729@gmail.com>
2026-02-01 22:15:16 +0000

    feat:javascript work, for interactive application form features

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("applicationForm");
+const list = document.getElementById("applicationList");
+const progressFill = document.getElementById("progressFill");
+const encouragement = document.getElementById("encouragement");
+const feedback = document.getElementById("feedback");
+
+let applications = [];
+
+const messages = [
+  "I see you've started, well done! Only focus on the next step.",
+  "Look at you go, you little rockstar!",
+  "Great! Try to submit at least one more. Future you will thank you!",
+  "Five already?! 100% main character energy!"
+];
